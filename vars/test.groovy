cb33fbec53b71ee57194a0d9391cced39c9af242
Nehashaik <shaikneha823@gmail.com>
2021-04-25 12:27:29 +0530

    Add files via upload

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,12 +1,55 @@
-  
-def call() {
-    stage("clone repo") {
-        steps {
-            script {
-                sh """
-                    echo "clone repo"
-                """
-            }
-        }
+def call(){
+
+pipeline {
+    agent any
+    options{
+        timestamps()
     }
+    stages {
+        stage("Parallel stages"){
+              parallel{
+                stage('Build'){
+                    steps{
+                        echo 'Build Stage done'
+                    }
+                }
+                 stage('Unit test') {
+                    steps {
+                        echo 'Unit test done'
+                    }
+                }
+              }
+        }
+        stage('Sonar') {
+                    steps {
+                        echo 'Sonar Stage done'
+                    }
+         }
+          stage('Docker build') {
+                    steps {
+                        echo 'Docker build stage done'
+                    }
+         }
+          stage('Deploy') {
+                    steps {
+                        echo 'Deploy stage done'
+                    }
+         }
+         stage('BDD') {
+             when{ anyOf{branch 'master' ; branch 'release-/*'}}
+                    steps {
+                        echo ' BDD stage done'
+                    }
+         }
+         stage('Nexus publish') {
+             when{ anyOf{branch 'master' ; branch 'release-/*'}}
+                    steps {
+                        echo 'Nexus publish Stage done'
+                    }
+         }
+        
+    }
+}
+
+
 }
