28f45b47652d892189c78c35cc32be6ff1b1de10
Nehashaik <shaikneha823@gmail.com>
2021-04-26 22:50:31 +0530

    Update example.groovy

--- vars/example.groovy
+++ vars/example.groovy
@@ -5,10 +5,6 @@ pipeline {
     options{
         timestamps()
     }
-  environment {
-    FULL_PATH_BRANCH = "${sh(script:'git name-rev --name-only HEAD', returnStdout: true)}"
-    GIT_BRANCH = FULL_PATH_BRANCH.substring(FULL_PATH_BRANCH.lastIndexOf('/') + 1, FULL_PATH_BRANCH.length())
-  }
 
 
     stages {
