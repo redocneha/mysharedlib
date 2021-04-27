48d650bf3567aae17d1b60de9a24ad95209c80e1
Nehashaik <shaikneha823@gmail.com>
2021-04-27 21:14:21 +0530

    Update example.groovy

--- vars/example.groovy
+++ vars/example.groovy
@@ -15,15 +15,7 @@ pipeline {
                 echo "FOO = ${env.FOO}"
                 echo "NAME = ${env.NAME}"
 
-                script {
-                    env.TEST_VARIABLE = "some test value"
-                }
-
-                echo "TEST_VARIABLE = ${env.TEST_VARIABLE}"
-
-                withEnv(["ANOTHER_ENV_VAR=here is some value"]) {
-                    echo "ANOTHER_ENV_VAR = ${env.ANOTHER_ENV_VAR}"
-                }
+             
             }
         }
     }
