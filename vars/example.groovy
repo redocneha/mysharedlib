e8294fa723b81b9ebc0ddee3900f2ad08c23500c
Nehashaik <shaikneha823@gmail.com>
2021-04-27 20:41:29 +0530

    Update example.groovy

--- vars/example.groovy
+++ vars/example.groovy
@@ -12,7 +12,8 @@ pipeline {
               parallel{
                 stage('Build'){
                     steps{
-                        echo GIT_BRANCH 
+                        echo "${GIT_BRANCH.replaceFirst(/^.*\//, '')}"
+ 
                         
                     }
                 }
